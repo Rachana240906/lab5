796df626f2bc72df513e33ea4ec471288bb75625
Rachana240906 <u24ai064@aid.svnit.ac.in>
2024-09-25 19:29:51 +0530

    Create rachana55.c

---
+++ rachana55.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],ocount=0,ecount=0,i;
+	printf("\nenter the numbers into array");
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("the array is:");
+	for(i=0;i<10;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		if(a[i]%2==0)
+		ecount=ecount+1;
+		else
+		ocount=ocount+1;
+	}
+	printf("\nthe total odd count is %d",ocount);
+	printf("\nthe total even count is %d",ecount);
+}
